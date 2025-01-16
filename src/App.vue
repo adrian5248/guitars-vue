6ee8bc04a45fdc108c9e0ca50a6f5f0a9a54b427
Adrian <adrian.duran.matias@gmail.com>
2025-01-16 11:48:48 -0600

    01 Guitarra, Header, Footer, Components

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Guitarra from './components/Guitarra.vue';
+import { db } from './data/guitarras'
+
+const guitarras =  ref(db)
+
+
+</script>
+
+<template>
+  <Header />
+        <main class="container-xl mt-5">
+            <h2 class="text-center">Nuestra Colección</h2>
+
+            <div class="row mt-5">
+                <Guitarra
+                    v-for="guitarra in guitarras"
+                    :guitarra="guitarra"
+                    />
+            </div>
+        </main>
+    <Footer/>
+</template>
+
+
+
+
+
